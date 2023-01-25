8df4942e77772eb9315586d53aa0c5fafae171d3
superstar <grozdanshopov320@gmail.com>
2023-01-26 03:47:22 +0900

    chat-app

--- client/src/Utilities/private-route.js
+++ client/src/Utilities/private-route.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import { Route, Navigate } from 'react-router-dom';
 
 import { authenticationService } from '../Services/authenticationService';
 
@@ -9,9 +9,9 @@ const PrivateRoute = ({ component: Component, ...rest }) => (
         render={props => {
             const currentUser = authenticationService.currentUserValue;
             if (!currentUser) {
-                // not logged in so redirect to login page with the return url
+                // not logged in so Navigate to login page with the return url
                 return (
-                    <Redirect
+                    <Navigate
                         to={{ pathname: '/', state: { from: props.location } }}
                     />
                 );
